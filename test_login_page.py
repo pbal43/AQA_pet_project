4b6a669cdec669d193d3089fe403172123e1b512
balyberdinpavel <pbal43@gmail.com>
2022-08-27 17:50:35 +0500

    added_test_login_page

---
+++ test_login_page.py
@@ -0,0 +1,8 @@
+from pages.login_page import *
+
+
+def test_guest_on_the_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    login_page = LoginPage(browser, link)
+    login_page.open()
+    login_page.should_be_login_page()
