63db080fb63de0f6b82b80530f9f375cfcce622b
balyberdinpavel <pbal43@gmail.com>
2022-08-27 16:25:07 +0500

    assert_on_login_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage():
 
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
+
+    """Проверка существования элемента"""
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):   #except(Наименование исключения - выбрасывает Пайтон при ошибке - можно подставлять - мб можно вообще без него - делал так давно))
+            return False
+        return True
 
     def open(self):
         self.browser.get(self.url)
